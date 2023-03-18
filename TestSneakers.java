1d1e0dbd7954c38c9c3a1764fc917f8719e2fc8c
Adalberto <21670125@itiguala.edu.mx>
2023-03-18 00:16:09 -0600

    Primer commit

---
+++ TestSneakers.java
@@ -0,0 +1,17 @@
+public class TestSneakers {
+    public static void main(String[] args) {
+    }
+
+    public class TheSneakers {
+        public static void main(String[] args) {
+            Sneakers tenis = new Sneakers();
+            tenis.setModel(2023);
+            tenis.setSize(8.0);
+            tenis.setColor("Blancos");
+            tenis.setMarca("Adidas");
+
+
+            tenis.toString();
+        }
+    }
+}
